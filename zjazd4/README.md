b04e984a596c499d38d4138e816dc0278d1a8432
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2022-11-30 11:46:17 +0100

    create fourth task

---
+++ zjazd4/README.md
@@ -0,0 +1,51 @@
+# Classification
+
+Authors: Zuzanna Ciborowska s20682 & Joanna Walkiewicz s20161
+Python project to create decision tree and svm classificator for Pima Indians Diabetes
+and Card Fraud Transactions Dataset
+
+Datasets can be found:
+- [Pima Indians Diabetes](https://machinelearningmastery.com/standard-machine-learning-datasets/)
+
+- [Card Fraud Transactions Dataset](https://www.kaggle.com/datasets/dhanushnarayananr/credit-card-fraud)
+
+File with Card Transactions used in the example has been shortened to reduce the program's running time
+
+
+To run the reccomender please install:
+
+- [Python 3.10](https://www.python.org/downloads/)
+
+- Pandas
+```bash
+pip install pandas
+```
+
+- Pydotplus
+```bash
+pip install pydotplus
+```
+
+- IPython
+```bash
+pip install ipython
+```
+
+- Sckit-learn
+```bash
+pip install -U scikit-learn
+```
+
+- Graphviv
+```bash
+pip install graphviz
+```
+
+
+The results of a sample runs of the program can be found in the files:
+
+- diabetes.png - decision tree for Pima Indians Diabetes dataset
+
+- fraud.png - decision tree for Card Fraud Transactions dataset 
+
+- svm results.png - SVM classificator results for both datasets
---
+++ zjazd4/main.py
@@ -0,0 +1,128 @@
+"""
+Authors: Zuzanna Ciborowska s20682 & Joanna Walkiewicz s20161
+Python project to create decision tree and svm classificator for Pima Indians Diabetes
+and Card Fraud Transactions Dataset
+System requirements:
+- Python 3.10
+- Pandas
+- Pydotplus
+- IPython
+- Sckit-learn
+- Graphviv
+"""
+import pandas as pd
+import pydotplus as pdp
+
+from IPython.display import Image
+from six import StringIO
+from sklearn import svm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+
+"""
+Loading the datasets: one with Pima Indians Diabetes data, and second with card transaction data
+"""
+dataPima = pd.read_csv('pima-indians-diabetes.csv')
+dataCard = pd.read_csv('card_transdata_smaller.csv')
+
+data_cols_pima = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction',
+              'Age']
+data_cols_card = ['distance_from_home', 'distance_from_last_transaction', 'ratio_to_median_purchase_price',
+             'repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']
+
+"""
+Splitting Pima Indians Diabetes data for 67% training and 33% test
+"""
+x = dataPima[data_cols_pima]
+y = dataPima.Outcome
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.33, random_state=1)
+
+"""
+Crating classifier for decision tree
+"""
+def tree(X_train, Y_train, X_test, Y_test):
+    classifier = DecisionTreeClassifier()
+    classifier = classifier.fit(X_train, Y_train)
+    y_pred = classifier.predict(X_test)
+
+    cm = confusion_matrix(Y_test.to_frame('Outcome'), y_pred, )
+    return classifier
+
+#pima indians
+"""
+Creating and making image of decision tree for Pima Indians Diabetes data
+"""
+classifier = tree(X_train, Y_train, X_test, Y_test)
+
+dot_data = StringIO()
+export_graphviz(classifier, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True, feature_names=data_cols_pima, class_names=['0', '1'])
+graph = pdp.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('diabetes.png')
+Image(graph.create_png())
+
+"""
+Create a svm Classifier for Pima Indians Diabetes data
+"""
+svc = svm.SVC(kernel='rbf', C=1, gamma=100).fit(x, y)
+
+"""
+Train the model using the training sets
+"""
+svc.fit(X_train, Y_train)
+
+"""
+Predict the response for test dataset
+"""
+y_pred = svc.predict(X_test)
+
+"""
+Model Accuracy: how often is the classifier correct?
+"""
+print("Accuracy for Pima Indians Diabetes data:",metrics.accuracy_score(Y_test, y_pred))
+
+
+#card transaction
+"""
+Splitting Card transaction data for 67% training and 33% test
+"""
+x = dataCard[data_cols_card]
+y = dataCard.fraud
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.33, random_state=1)
+
+"""
+Creating and making image of decision tree for Card transaction data
+"""
+classifier = tree(X_train, Y_train, X_test, Y_test)
+
+dot_data = StringIO()
+export_graphviz(classifier, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True, feature_names=data_cols_card, class_names=['0', '1'])
+graph = pdp.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('fraud.png')
+Image(graph.create_png())
+
+"""
+Create a svm Classifier for Card transaction data
+"""
+svc = svm.SVC(kernel='rbf', C=1, gamma=100).fit(x, y)
+
+"""
+Train the model using the training sets
+"""
+svc.fit(X_train, Y_train)
+
+"""
+Predict the response for test dataset
+"""
+y_pred = svc.predict(X_test)
+
+"""
+Model Accuracy: how often is the classifier correct?
+"""
+print("Accuracy for Card transaction data:",metrics.accuracy_score(Y_test, y_pred))
